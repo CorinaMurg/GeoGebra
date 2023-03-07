df33d0069fd18267c2651c6ad8bb158a34b12cef
Cor-Ina <115652409+Cor-Ina@users.noreply.github.com>
2023-03-06 19:35:38 -0500

    Update GeoGebra folder

--- matchTheLinearEquationJS copy/intersectLineWithAxes.js
+++
@@ -1,6 +0,0 @@
-// var xaxis = ggbApplet.evalCommandGetLabels("y=0");
-    // ggbApplet.setColor(xaxis, 255, 0, 0);
-
-    //var xIntercept = ggbApplet.evalCommandGetLabels("Intersect(newLine, xaxis)");
-    // ggbApplet.setColor(xIntercept, 0, 0, 255);
-    // ggbApplet.setLabelVisible(xIntercept, true);
